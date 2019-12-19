c966bee61524fdc4e9037a89b1bd2e75ad509c94
Gaetan CESARO <EGCESARO@cafat.nc>
2019-12-19 11:35:13 +1100

    feat(MEA-394): Ajout tests CLI => GPP AdressePostale

--- src/Config.py
+++ src/Config.py
@@ -695,6 +695,190 @@ TESTS = [
                 }
             }
         }
+    },
+    {
+        "name": "PostGreToPostGre-CliToCli-AdressePostale",
+        "sleeptime": 6,
+        "in": {
+            "type": "REST",
+            "server": "SPRINGBOOT",
+            "operation": {
+                "command": "/s-gen-cli-2.0/personne-physique/540003/contact",
+                "data": {
+                    "adressePostale": {
+                        "type": "domicile",
+                        "ligneDesserte": {
+                            "type": "reference",
+                            "id": "28871"
+                        },
+                        "lignePays": {
+                            "code": "540"
+                        },
+                        "ligneComplement": "RES LES CITRONS APT 3",
+                        "ligneLieuDit": {
+                            "type": "libre",
+                            "libelle": "TRIANON"
+                        },
+                        "ligneVoie": {
+                            "type": "reference",
+                            "numero": "16",
+                            "id": 502250
+                        }
+                    },
+                    "telephoneFixe": {
+                        "type": "fixe",
+                        "numero": "0"
+                    },
+                    "telephoneMobile": {
+                        "type": "mobile",
+                        "numero": "522933"
+                    },
+                    "typeContact": "ASSURE"
+                }
+            }
+        },
+        "out": {
+            "type": "SQL",
+            "server": "POSTGRE",
+            "operation": {
+                "command": "select 1 from sgencli.cli_contact c, sgencli.cli_personne_physique pp where c.id_fk_client = pp.id_fk_client and pp.matricule = '540003' and c.adresse_postale like '%CITRONS%16,%HENDRIX%TRIANON%98809 %DORE%'",
+                "data": ""
+            },
+            "expected": {
+                "attribute": "1",
+                "value": "1"
+            }
+        },
+        "rollback": {
+            "type": "REST",
+            "server": "SPRINGBOOT",
+            "operation": {
+                "command": "/s-gen-cli-2.0/personne-physique/540003/contact",
+                "data": {
+                    "adressePostale": {
+                        "type": "domicile",
+                        "ligneDesserte": {
+                            "type": "reference",
+                            "id": "6833"
+                        },
+                        "lignePays": {
+                            "code": "540"
+                        },
+                        "ligneComplement": "RES LES BAMBOUS APT 3",
+                        "ligneLieuDit": {
+                            "type": "libre",
+                            "libelle": "VAL PLAISANCE"
+                        },
+                        "ligneVoie": {
+                            "type": "reference",
+                            "numero": "15",
+                            "id": 502249
+                        }
+                    },
+                    "telephoneFixe": {
+                        "type": "fixe",
+                        "numero": "0"
+                    },
+                    "telephoneMobile": {
+                        "type": "mobile",
+                        "numero": "522933"
+                    },
+                    "typeContact": "ASSURE"
+                }
+            }
+        }
+    },
+    {
+        "name": "PostGreToPostGre-CliToGpp-AdressePostale",
+        "sleeptime": 6,
+        "in": {
+            "type": "REST",
+            "server": "SPRINGBOOT",
+            "operation": {
+                "command": "/s-gen-cli-2.0/personne-physique/540003/contact",
+                "data": {
+                    "adressePostale": {
+                        "type": "domicile",
+                        "ligneDesserte": {
+                            "type": "reference",
+                            "id": "28871"
+                        },
+                        "lignePays": {
+                            "code": "540"
+                        },
+                        "ligneComplement": "RES LES CITRONS APT 3",
+                        "ligneLieuDit": {
+                            "type": "libre",
+                            "libelle": "TRIANON"
+                        },
+                        "ligneVoie": {
+                            "type": "reference",
+                            "numero": "16",
+                            "id": 502250
+                        }
+                    },
+                    "telephoneFixe": {
+                        "type": "fixe",
+                        "numero": "0"
+                    },
+                    "telephoneMobile": {
+                        "type": "mobile",
+                        "numero": "522933"
+                    },
+                    "typeContact": "ASSURE"
+                }
+            }
+        },
+        "out": {
+            "type": "SQL",
+            "server": "POSTGRE",
+            "operation": {
+                "command": "select 1 from sgengpp.gpp_adresse_domicile ad, sgengpp.gpp_moyen_contact mc, sgengpp.gpp_personne_physique pp where ad.id_fk_ap = mc.id and mc.date_fin_validite is null and mc.fk_personne_physique = pp.numero_interne and pp.matricule = 540003 and ad.complement like '%CITRONS%' and ad.fk_voie = 502250 and ad.lieu_dit_libre = 'TRIANON' and numero_voie = '16'",
+                "data": ""
+            },
+            "expected": {
+                "attribute": "1",
+                "value": "1"
+            }
+        },
+        "rollback": {
+            "type": "REST",
+            "server": "SPRINGBOOT",
+            "operation": {
+                "command": "/s-gen-cli-2.0/personne-physique/540003/contact",
+                "data": {
+                    "adressePostale": {
+                        "type": "domicile",
+                        "ligneDesserte": {
+                            "type": "reference",
+                            "id": "6833"
+                        },
+                        "lignePays": {
+                            "code": "540"
+                        },
+                        "ligneComplement": "RES LES BAMBOUS APT 3",
+                        "ligneLieuDit": {
+                            "type": "libre",
+                            "libelle": "VAL PLAISANCE"
+                        },
+                        "ligneVoie": {
+                            "type": "reference",
+                            "numero": "15",
+                            "id": 502249
+                        }
+                    },
+                    "telephoneFixe": {
+                        "type": "fixe",
+                        "numero": "0"
+                    },
+                    "telephoneMobile": {
+                        "type": "mobile",
+                        "numero": "522933"
+                    },
+                    "typeContact": "ASSURE"
+                }
+            }
+        }
     }
 ]
     
